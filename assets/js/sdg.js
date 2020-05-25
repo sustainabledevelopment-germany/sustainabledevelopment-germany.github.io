fec6bede59ae19fe80fb11b747c19cdbe66ada2e
Travis CI <>
2020-05-25 10:31:12 +0000

    Deploy 4c55a0f from branch master

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1099,7 +1099,9 @@ var indicatorDataStore = function(dataUrl) {
 
       getCombinationDescription = function(combination) {
         return _.map(Object.keys(combination), function(key) {
+          console.log("key: ",key);
           return translations.t(combination[key]);
+          //return translations.t(key) + ": " + translations.t(combination[key]);
           //return key + ' ' + combination[key];
         }).join(', ');
       },
